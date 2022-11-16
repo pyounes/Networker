c34a8e77a57783b5266c5c56ef2f37429f21041a
Pierre Y <pyounes@gmail.com>
2022-11-16 23:55:05 +0200

    adding `NWActivityIndicator` for the use of any activity indicator or loader

---
+++ Sources/Networker/Protocols/NWActivityIndicator.swift
@@ -0,0 +1,13 @@
+//
+//  NWActivityIndicator.swift
+//  
+//
+//  Created by Pierre Younes on 14/11/2022.
+//
+
+import Foundation
+
+public protocol NWActivityIndicator {
+  func addLoader()
+  func removeLoader()
+}
