d0978ec389ec00cd16a0465dbf40e251e044a084
Pierre Y <pyounes@gmail.com>
2022-11-16 23:59:52 +0200

    adding initializer for the Networker

--- Sources/Networker/Networker.swift
+++ Sources/Networker/Networker.swift
@@ -1,6 +1,20 @@
-public struct Networker {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
+public final class Networker {
+  
+  private let logger: NWLogger
+  private let activityIndicator: NWActivityIndicator
+  private let session: NWSessionConfiguration
+  
+  public init(
+    session: NWSessionConfiguration,
+    logger: NWLogger,
+    activityIndicator: NWActivityIndicator
+  ) {
+    self.logger = logger
+    self.activityIndicator = activityIndicator
+    self.session = session
+  }
+  
+  
 }
