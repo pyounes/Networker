07cced660ba6cddbe9b29e727ad9bd81a1738fd6
Pierre Y <pyounes@gmail.com>
2022-11-17 22:37:51 +0200

    added test case

--- Tests/NetworkerTests/NetworkerTests.swift
+++ Tests/NetworkerTests/NetworkerTests.swift
@@ -1,63 +1,85 @@
 import XCTest
 @testable import Networker
 
-struct Text: Codable {
-    let id: String
-    let text: String
+struct Root: Decodable {
+  let status: Bool
+  let totalCount: Int?
+  let data: Text
+  let title: String
+  let message: String
+  let developerMessage: String
+  let responseCode: Int
 }
 
-enum TermsAPI: NWRequestBuilder {
-    
-    case getTerms
-    
-    var baseURL: URL {
-        return URL(string: "https://api-merchantapp.montypay.com")!
-    }
-    
-    var path: String {
-        return "/merchant-app/termsAndConditions"
-    }
-    
-    var headers: [String : String]? {
-        ["testing":"headers"]
-    }
-    
-    var query: [String : String?]? {
-        ["testing":"query"]
-    }
-    
-    var parameters: [String : Any]?{
-        ["testing":"parameters"]
-    }
-    
-    var withToken: Bool {
-        false
-    }
-    
-    var token: String {
-        "TESTING_TOKEN-123ouiwkdsjv"
-    }
-    
-    var acceptableStatusCodes: ClosedRange<Int> {
-        return 200...299
-    }
-    
-    var httpMethod: NWMethod {
-        .get
-    }
-    
+struct Text: Decodable {
+  let id: String
+  let text: String
 }
+
+enum TermsAPI: NWRequest {
+  
+  case getTerms
+  
+  var baseURL: URL {
+    return URL(string: "https://api-merchantapp.montypay.com")!
+  }
+  
+  var path: String {
+    return "/merchant-app/termsAndConditions"
+  }
+  
+//  var headers: [String : String]? {
+//    ["testing":"headers"]
+//  }
+  
+//  var query: [String : String?]? {
+//    ["testing":"query"]
+//  }
+  
+//  var parameters: [String : Any]?{
+//    ["testing":"parameters"]
+//  }
+  
+  var withToken: Bool {
+    false
+  }
+  
+  var token: String {
+    "TESTING_TOKEN-123ouiwkdsjv"
+  }
+  
+  var acceptableStatusCodes: ClosedRange<Int> {
+    return 200...299
+  }
+  
+  var httpMethod: NWMethod {
+    .get
+  }
+  
+}
+
+fileprivate class session: NWSessionConfiguration {}
+fileprivate class logger: NWLogger {}
+fileprivate class activityIndicator: NWActivityIndicator {}
 
 final class NetworkerTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        
-        let builder = TermsAPI.getTerms
-        
-        
-        dump(builder.token)
+  func testExample() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    
+    let request = TermsAPI.getTerms
+    let apiCaller = Networker(session: session(), logger: logger(), activityIndicator: activityIndicator())
+    
+    let exp = expectation(description: "wait for call")
+    
+    apiCaller.taskHandler(request: request, response: Root.self) { result in
+//      dump(result)
+      exp.fulfill()
     }
     
+    wait(for: [exp], timeout: 2.0)
+
+  }
+  
 }
