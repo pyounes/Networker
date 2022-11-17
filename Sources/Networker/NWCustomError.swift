1bff6879b60b92f66db545868ead4af74c230c55
Pierre Y <pyounes@gmail.com>
2022-11-17 22:35:32 +0200

    added custom errors

---
+++ Sources/Networker/NWCustomError.swift
@@ -0,0 +1,46 @@
+//
+//  NWCustomError.swift
+//  
+//
+//  Created by Pierre Younes on 17/11/2022.
+//
+
+import Foundation
+
+public enum NWCustomError: Error {
+  case serverDown
+  case noInternet
+  case unauthorized
+  case sessionExpired
+  case emptyData
+  case authorizationError
+  case endPointServerDown
+  case invalidStatusCode(Int)
+  case invalidResponse
+}
+
+extension NWCustomError: LocalizedError {
+  
+  var localizedDescription: String {
+    switch self {
+    case .serverDown:
+      return "Server or Endpoint Unavailable"
+    case .noInternet:
+      return "Your Internet connection appears to be offline"
+    case .unauthorized:
+      return "Unauthorized Access"
+    case .sessionExpired:
+      return "Session Expired"
+    case .authorizationError:
+      return "URL Path Not Found"
+    case .endPointServerDown:
+      return "Endpoint Server Down"
+    case .emptyData:
+      return "Empty Data"
+    case let .invalidStatusCode(code):
+      return "Expected 200 ~ 299, but got \(code.description) instead"
+    case .invalidResponse:
+      return "Request did not return HttpURLResponse"
+    }
+  }
+}
