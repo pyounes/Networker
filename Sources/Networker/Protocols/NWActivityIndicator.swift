0f497d565f6733397cd3eac62b56cd18fce07938
Pierre Younes <pierre.younes@drapp.me>
2023-04-19 15:38:27 +0300

    adding podspec to support CocoaPods

--- Sources/Networker/Protocols/NWActivityIndicator.swift
+++ Sources/Networker/Protocols/NWActivityIndicator.swift
@@ -10,35 +10,35 @@ public protocol NWActivityIndicator {
   func removeLoader()
 }
 
-
-#if canImport(UIKit)
-import UIKit
-
-public final class NWDefaultActivityIndicator: NWActivityIndicator {
-  
-  private let activityIndicator = UIActivityIndicatorView(style: .large)
-  private let view: UIView = UIView(frame: CGRect(x: 0, y: 0, width: Screen.width, height: Screen.height))
-  private let blur: UIColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
-  
-  
-  func addLoader() {
-    removeLoader()
-    
-    view.backgroundColor = withBackground ? blur : .clear
-    activityIndicator.center = view.center
-    activityIndicator.startAnimating()
-    view.addSubview(activityIndicator)
-    
-    guard let window = UIApplication.shared.windows.last else { return }
-    
-    window.addSubview(view)
-  }
-  
-  func removeLoader() {
-    view.removeFromSuperview()
-    activityIndicator.stopAnimating()
-  }
-  
-}
-
-#endif
+//
+//#if canImport(UIKit)
+//import UIKit
+//
+//public final class NWDefaultActivityIndicator: NWActivityIndicator {
+//  
+//  private let activityIndicator = UIActivityIndicatorView(style: .large)
+//  private let view: UIView = UIView(frame: CGRect(x: 0, y: 0, width: Screen.width, height: Screen.height))
+//  private let blur: UIColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+//  
+//  
+//  public func addLoader() {
+//    removeLoader()
+//    
+//    view.backgroundColor = withBackground ? blur : .clear
+//    activityIndicator.center = view.center
+//    activityIndicator.startAnimating()
+//    view.addSubview(activityIndicator)
+//    
+//    guard let window = UIApplication.shared.windows.last else { return }
+//    
+//    window.addSubview(view)
+//  }
+//  
+//  public func removeLoader() {
+//    view.removeFromSuperview()
+//    activityIndicator.stopAnimating()
+//  }
+//  
+//}
+//
+//#endif
